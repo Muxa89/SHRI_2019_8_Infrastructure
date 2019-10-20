9f1f96743e61f79db0ead7e1a021c474f6db738b
Muxa89 <muxa.89@inbox.ru>
2019-10-20 09:36:28 +0400

    Added notify_build_result endpoint.

--- server/server.ts
+++ server/server.ts
@@ -12,10 +12,19 @@ interface Build {
   id: number;
   hash: string;
   state: BuildState;
+  stdout: string;
+  stderr: string;
 }
 
 interface BuildMap {
   [id: number]: Build;
+}
+
+interface BuildResult {
+  id: number;
+  state: BuildState;
+  stdout: string;
+  stderr: string;
 }
 
 enum AgentState {
@@ -30,33 +39,42 @@ interface Agent {
   state: AgentState;
 }
 
+interface AgentBuild {
+  [buildId: number]: Agent;
+}
+
 function initUi(app: Application, builds: BuildMap): void {
   app.set('views', './server/public/templates');
   app.set('view engine', 'pug');
 
-  app.get('/', (req: Request, res: Response) => res.render('index.pug'));
+  app.get('/', (req, res) => res.render('index.pug'));
 
-  app.get('/build/:buildId', (req: Request, res: Response) =>
+  app.get('/build/:buildId', (req, res) =>
     res.render('build.pug', { buildId: req.params.buildId })
   );
 
   app.use('/js', express.static('server/public/js'));
   app.use('/js', express.static('node_modules/axios/dist'));
 
-  app.post('/', (req: Request, res: Response) => {
+  app.post('/', (req, res) => {
     console.log(req.body);
     res.sendStatus(200);
   });
 
-  app.get('/status', (req: Request, res: Response) =>
+  app.get('/status', (req, res) =>
     res.send({
       builds,
     })
   );
 }
 
-function initAgentApi(app: Application, agents: Array<Agent>): void {
-  app.post('/notify_agent', req => {
+function initAgentApi(
+  app: Application,
+  agents: Array<Agent>,
+  builds: BuildMap,
+  agentBuild: AgentBuild
+): void {
+  app.post('/notify_agent', (req, res) => {
     const { host, port } = req.body;
     agents.push({
       host,
@@ -67,6 +85,23 @@ function initAgentApi(app: Application, agents: Array<Agent>): void {
       `Agent #${agents.length -
         1} on ${host}:${port} was successfully registered and is waiting for commands.`
     );
+    res.sendStatus(200);
+  });
+
+  app.post('/notify_build_result', (req, res) => {
+    const { id, state, stdout, stderr } = req.body as BuildResult;
+    const build = builds[id];
+    build.state = state;
+    build.stdout = stdout;
+    build.stderr = stderr;
+
+    const agent = agentBuild[id];
+    console.log(
+      `Agent on ${agent.host}:${agent.port} finished task with state: ${state}`
+    );
+    delete agentBuild[id];
+
+    res.sendStatus(200);
   });
 }
 
@@ -75,26 +110,26 @@ const builds = {
     id: 1,
     hash: '123',
     state: BuildState.SUCCESS,
+    stdout: '',
+    stderr: '',
   },
   2: {
     id: 2,
     hash: '223',
     state: BuildState.IN_PROGRESS,
+    stdout: '',
+    stderr: '',
   },
 };
-
 const agents: Array<Agent> = [];
+const agentBuild: AgentBuild = {};
 
 const app = express();
 app.use(express.json());
 
 initUi(app, builds);
-initAgentApi(app, agents);
+initAgentApi(app, agents, builds, agentBuild);
 
 app.listen(config.port, () =>
   console.log(`Server is listening on port ${config.port}`)
 );
-
-// app.post('/notify_build_result', req => {
-//   const params = req.body;
-// });
