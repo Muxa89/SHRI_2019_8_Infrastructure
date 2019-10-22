ca219f017ca47cd1e47f152fc1a76394ef82ea0f
Muxa89 <muxa.89@inbox.ru>
2019-10-22 13:38:29 +0400

    Added command execution.

--- agent/agent.ts
+++ agent/agent.ts
@@ -1,7 +1,11 @@
-import * as express from 'express';
 import { config } from './config';
+import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
+
+import * as express from 'express';
 import axios from 'axios';
+
 import { BuildState, BuildCommand, BuildResult } from '../server/executor';
+import { Readable } from 'stream';
 
 const app = express();
 app.use(express.json());
@@ -9,18 +13,11 @@ app.use(express.json());
 app.post('/build', (req, res) => {
   const command: BuildCommand = req.body;
   console.log(`Received build command: ${JSON.stringify(command)}`);
+  console.log('Build started...');
+  console.log('');
   res.sendStatus(200);
 
-  run(command)
-    .then((buildResult: BuildResult) => sendBuildResult(buildResult))
-    .catch(stderr =>
-      sendBuildResult({
-        buildId: command.buildId,
-        state: BuildState.FAILURE,
-        stdout: '',
-        stderr: stderr,
-      })
-    );
+  run(command).then((buildResult: BuildResult) => sendBuildResult(buildResult));
 });
 
 app.listen(config.port, () =>
@@ -57,21 +54,147 @@ const registration = (() => {
 })();
 registration();
 
-const run = (command: BuildCommand) =>
-  new Promise((resolve, reject) => {
-    setTimeout(
-      () =>
-        resolve({
-          buildId: command.buildId,
-          state: BuildState.SUCCESS,
-          stdout: 'success',
-          stderr: '',
-        } as BuildResult),
-      3000
-    );
-  });
+interface RunResult {
+  code: number;
+  stdout: StringBuffer;
+  stderr: StringBuffer;
+}
+
+class StringBuffer {
+  private buffer: string;
+
+  constructor() {
+    this.buffer = '';
+  }
+
+  append(data: string) {
+    this.buffer += data;
+  }
+
+  toString() {
+    return this.buffer;
+  }
+}
+
+const consoleLogAndBuffer = (prefix: string, stringBuffer: StringBuffer) => (
+  data: string
+) => {
+  console.log(`${prefix}: ${data}`);
+  stringBuffer.append(data);
+};
+
+const processStream = (stream: Readable, prefix: string) => {
+  const buffer = new StringBuffer();
+  stream.setEncoding('utf8');
+  stream.on('data', consoleLogAndBuffer(prefix, buffer));
+  return buffer;
+};
+
+const bufferizeChildProcessOutput = (
+  childProcess: ChildProcessWithoutNullStreams,
+  prefix: string
+): [StringBuffer, StringBuffer] => {
+  return [
+    processStream(childProcess.stdout, `${prefix} STDOUT`),
+    processStream(childProcess.stderr, `${prefix} STDERR`),
+  ];
+};
+
+const runResultToBuildResult = (
+  runResult: RunResult,
+  command: BuildCommand
+): BuildResult => ({
+  buildId: command.buildId,
+  state: runResult.code === 0 ? BuildState.SUCCESS : BuildState.FAILURE,
+  stdout: runResult.stdout.toString(),
+  stderr: runResult.stderr.toString(),
+});
+
+const run = (command: BuildCommand) => {
+  // git checkout
+  return (
+    new Promise((res, rej) => {
+      const checkout = spawn('git', ['checkout', command.hash], {
+        cwd: command.path,
+      });
+
+      const [stdout, stderr] = bufferizeChildProcessOutput(
+        checkout,
+        'CHECKOUT'
+      );
+
+      checkout.on('close', code => {
+        const runResult: RunResult = {
+          code,
+          stdout,
+          stderr,
+        };
+
+        if (code !== 0) {
+          rej(runResult);
+        } else {
+          res(runResult);
+        }
+      });
+    })
+      // run provided command
+      .then(
+        (runResult: RunResult) =>
+          new Promise((res, rej) => {
+            const commandArr = command.command.split(' ');
+
+            if (process.platform === 'win32') {
+              commandArr[0] = `${commandArr[0]}.cmd`;
+            }
+
+            const build = spawn(
+              commandArr[0],
+              commandArr.slice(1, commandArr.length),
+              {
+                cwd: command.path,
+              }
+            );
+
+            const [stdout, stderr] = bufferizeChildProcessOutput(
+              build,
+              'BUILD'
+            );
+
+            const newResult = Object.assign({}, runResult);
+
+            build.on('close', code => {
+              newResult.code = code;
+              newResult.stdout.append(stdout.toString());
+              newResult.stderr.append(stderr.toString());
+
+              if (code !== 0) {
+                rej(newResult);
+              } else {
+                res(newResult);
+              }
+            });
+
+            build.on('error', err => {
+              newResult.code = -1;
+              newResult.stderr.append(
+                `BUILD Error: ${err.name}\n${err.message}\n${err.stack}\n`
+              );
+              rej(newResult);
+            });
+          })
+      )
+      .then((runResult: RunResult) =>
+        runResultToBuildResult(runResult, command)
+      )
+      .catch((runResult: RunResult) =>
+        runResultToBuildResult(runResult, command)
+      )
+  );
+};
 
 const sendBuildResult = (buildResult: BuildResult) => {
+  console.log(`Sending build result: ${buildResult}`);
+
   axios.post(
     `http://localhost:${config.hostPort}/notify_build_result`,
     buildResult
